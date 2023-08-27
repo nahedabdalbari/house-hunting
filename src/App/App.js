81ae8a5e9ad2a75db72fb67902e52fbea5433e57
nahedabdalbari <nahed.abdalbari.kytc@gmail.com>
2023-08-27 16:48:23 +0300

    test

--- src/App/App.js
+++ src/App/App.js
@@ -5,7 +5,8 @@ import About from '../Pages/About';
 import Details from '../Pages/Detailes';
 import SearchPage from '../Pages/SearchPage';
 import NotFound from '../Pages/NotFound';
-import Login from '../Pages/Login';
+// import Login from '../Pages/Login';
+import Loginpop from '../Components/Loginpop';
 
 
 function App() {
@@ -19,7 +20,10 @@ function App() {
              <Route path={`/detailes/:id`} element={<Details />} />
         
           <Route path="/SearchPage" element={<SearchPage />} />
-          <Route path="/Login" element={<Login/>} />
+          {/* <Route path="/Login" element={<Login/>} /> */}
+
+          <Route path="/Loginpop" element={<Loginpop/>} />
+
 
           <Route path='*' element={<NotFound />}/>
         
